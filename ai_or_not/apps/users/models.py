0f0c917dc23edff4741a247477e421713f886b7e
Jeyw1n <j3ywin@gmail.com>
2024-09-01 16:52:13 +0300

    Создано приложение для аутентификации

---
+++ ai_or_not/apps/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class UserAccount(AbstractUser):
+    """
+    Представляет обычного пользователя
+    """
+    # Пока пусто =(
+    pass
+
